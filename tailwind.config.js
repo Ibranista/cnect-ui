a894ad8454705ff79452282697e5b07f3d24d7e6
zakisudev <zakisudev@gmail.com>
2024-10-05 14:49:11 +0300

    ft: basic header component and project setup.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,ts,scss}"],
+  theme: {
+    extend: {
+      colors: {
+        primary_brand: "#033C6D",
+        light_brand: "#f5f5ee",
+        secondary_brand: "#148AD6",
+        bg_gray: "#F7F7F7",
+        light_gray: "#BFBFBF",
+        inactive: "#A4A4A4",
+      },
+    },
+  },
+  plugins: [],
+};
